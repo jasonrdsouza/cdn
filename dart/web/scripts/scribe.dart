e068a9a9a661a27a88853015dbf71fc793aa2286
Jason Dsouza <jasonrdsouza@gmail.com>
2020-04-29 23:53:37 -0400

    Add basic scribe frontend

--- dart/web/scripts/scribe.dart
+++ dart/web/scripts/scribe.dart
@@ -1,6 +1,24 @@
+import 'dart:convert';
 import 'dart:html';
 
 void main() async {
   print('Article Scribe Awoken');
-  // https://us-central1-dsouza-proving-ground.cloudfunctions.net/scribe/simplify
+  String scribeUrl = "https://us-central1-dsouza-proving-ground.cloudfunctions.net/scribe/simplify";
+
+  ButtonElement submitButton = querySelector('#submit');
+  InputElement urlInput = querySelector('#sourceUrl');
+  HeadingElement articleTitle = querySelector('#articleTitle');
+  DivElement articleContents = querySelector('#articleContents');
+
+  submitButton.onClick.listen((_) {
+    var body = {'page': urlInput.value.trim()};
+    var headers = {'Content-Type': 'application/json'};
+    HttpRequest.request(scribeUrl, method: 'POST', requestHeaders: headers, sendData: json.encode(body))
+      .then((HttpRequest resp) {
+        var readableResult = json.decode(resp.responseText);
+        articleTitle.text = readableResult['title'];
+        articleContents.setInnerHtml(readableResult['content']);
+      });
+  });
+
 }
