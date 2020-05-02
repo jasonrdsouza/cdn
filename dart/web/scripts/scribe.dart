8c24f66ab869e308ee51c0c45378cab1c8b350c1
Jason Dsouza <jasonrdsouza@gmail.com>
2020-05-01 23:50:54 -0400

    Add read times/ word count to scribe

--- dart/web/scripts/scribe.dart
+++ dart/web/scripts/scribe.dart
@@ -4,10 +4,12 @@ import 'dart:html';
 void main() async {
   print('Article Scribe Awoken');
   String scribeUrl = "https://us-central1-dsouza-proving-ground.cloudfunctions.net/scribe/simplify";
+  // String scribeUrl = "http://127.0.0.1:8081/simplify";
 
   ButtonElement submitButton = querySelector('#submit');
   InputElement urlInput = querySelector('#sourceUrl');
   HeadingElement articleTitle = querySelector('#articleTitle');
+  HeadingElement articleMetrics = querySelector('#articleMetrics');
   DivElement articleContents = querySelector('#articleContents');
 
   submitButton.onClick.listen((_) {
@@ -17,8 +19,22 @@ void main() async {
       .then((HttpRequest resp) {
         var readableResult = json.decode(resp.responseText);
         articleTitle.text = readableResult['title'];
+        articleMetrics.text = produceMetricText(readableResult['textContent']);
         articleContents.setInnerHtml(readableResult['content']);
       });
   });
+}
 
+int countWords(articleContents) {
+  return articleContents.split(new RegExp(r'\s+')).length;
+}
+
+double calculateReadTime(int numWords) {
+  return numWords / 200;
+}
+
+String produceMetricText(String articleContents) {
+  int numWords = countWords(articleContents);
+  int readTimeMins = calculateReadTime(numWords).round();
+  return '(${readTimeMins} minute read | ${numWords} words)';
 }
