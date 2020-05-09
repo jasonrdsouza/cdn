0bfb90c48eaff1396cd1e61e10c513a3d3795898
Jason Dsouza <jasonrdsouza@gmail.com>
2020-05-08 22:07:45 -0400

    Add url query parameter for linkability
    Also relax html sanitization constraints to allow links and images to
    come through. Still need to figure out how to fix relative links and
    images though.
    
    Finally, wrap url submission input box in a form so that users can hit
    enter instead of clicking "fetch"

--- dart/web/scripts/scribe.dart
+++ dart/web/scripts/scribe.dart
@@ -4,15 +4,18 @@ import 'dart:html';
 void main() async {
   print('Article Scribe Awoken');
   String scribeUrl = "https://us-central1-dsouza-proving-ground.cloudfunctions.net/scribe/simplify";
-  // String scribeUrl = "http://127.0.0.1:8081/simplify";
+  // String scribeUrl = "http://localhost:8081/simplify";
 
+  FormElement urlForm = querySelector('#urlForm');
   ButtonElement submitButton = querySelector('#submit');
   InputElement urlInput = querySelector('#sourceUrl');
   HeadingElement articleTitle = querySelector('#articleTitle');
   HeadingElement articleMetrics = querySelector('#articleMetrics');
   DivElement articleContents = querySelector('#articleContents');
 
-  submitButton.onClick.listen((_) {
+  urlForm.onSubmit.listen((Event e) {
+    e.preventDefault(); // prevent page from reloading
+
     var body = {'page': urlInput.value.trim()};
     var headers = {'Content-Type': 'application/json'};
     HttpRequest.request(scribeUrl, method: 'POST', requestHeaders: headers, sendData: json.encode(body))
@@ -20,9 +23,42 @@ void main() async {
         var readableResult = json.decode(resp.responseText);
         articleTitle.text = readableResult['title'];
         articleMetrics.text = produceMetricText(readableResult['textContent']);
-        articleContents.setInnerHtml(readableResult['content']);
+        transcribeArticleContents(articleContents, readableResult['content']);
       });
   });
+
+  fetchInitialUrl(urlInput, submitButton);
+}
+
+class AllowAllUriPolicy implements UriPolicy {
+  @override
+  bool allowsUri(String uri) {
+    return true;
+  }
+}
+
+transcribeArticleContents(DivElement articleDiv, String articleContents) {
+  UriPolicy allowAllUris = AllowAllUriPolicy();
+  NodeValidator validator = NodeValidatorBuilder.common()
+    ..allowTextElements()
+    ..allowNavigation(allowAllUris)
+    ..allowImages(allowAllUris)
+    ..allowSvg();
+
+  articleDiv.setInnerHtml(articleContents, validator: validator);
+}
+
+fetchInitialUrl(InputElement urlInput, ButtonElement submitButton) {
+  String key = 'url';
+  if (Uri.base.queryParameters.containsKey(key)) {
+    Uri initialUrl = Uri.parse(Uri.base.queryParameters['url'].trim());
+    if (initialUrl.hasScheme) {
+      urlInput.value = initialUrl.toString();
+    } else {
+      urlInput.value = 'http://${initialUrl}';
+    }
+    submitButton.click();
+  }
 }
 
 int countWords(articleContents) {
